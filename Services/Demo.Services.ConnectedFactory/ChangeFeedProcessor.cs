a4c9c173eec8d93d2cd83f060cb5c9b6621b7079
Scott Rutz <scrutz@microsoft.com>
2023-06-20 15:38:12 +0000

    cp

---
+++ Services/Demo.Services.ConnectedFactory/ChangeFeedProcessor.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Demo.Services.ConnectedFactory.Data;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Services.ConnectedFactory
+{
+    public static class ChangeFeedProcessor
+    {
+        [FunctionName("ChangeFeedProcessor")]
+        public static void Run([CosmosDBTrigger(databaseName: "ConnectedFactory", containerName: "Measurement", 
+            Connection = "CosmosDBConnection",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)]IReadOnlyList<Measurement> input,
+            ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                log.LogInformation("First document Id " + input[0].Id);
+            }
+        }
+    }
+}
---
+++ Services/Demo.Services.ConnectedFactory/Data/Measurement.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Services.ConnectedFactory.Data
+{
+    public class Measurement
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("extendedInformation")]
+        public string ExtendedInformation { get; set; }
+
+    }
+}
